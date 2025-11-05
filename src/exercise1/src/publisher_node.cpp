8c07dc4a869643db85444474e85518bc183a61af
ahemyu <hansovic7@gmail.com>
2025-11-05 20:48:41 +0100

    ex1: publishing and subscribing to int messages

--- src/exercise1/src/publisher_node.cpp
+++ src/exercise1/src/publisher_node.cpp
@@ -6,10 +6,10 @@
 
 using namespace std::chrono_literals; // adds user-defined literals for time durations.
 
-class Publisher : public rclcpp::Node {
+class Talker : public rclcpp::Node {
 //TODO: implement the constructor
 public: 
-  Publisher(): Node("MyPublisher"), counter_(0)
+  Talker(): Node("Talker"), counter_(0)
   {
     // Create a Publisher node named Talker in there,
     // which publishes an "int64" message (from the ROS package "std_msgs") under the topic "first_test_topic" with 20 Hz, counting up from 0 to 50 and
@@ -17,7 +17,7 @@ public:
     publisher_ = this->create_publisher<std_msgs::msg::Int64>("first_test_topic", 10); //TODO: don't know what the 10 does   
       
     timer_ = this->create_wall_timer(
-      50ms, std::bind(&Publisher::timer_callback, this)); // create a timer that fires every 500 milliseconds, and when it fires, call the timer_callback() method on this object instance.
+      50ms, std::bind(&Talker::timer_callback, this)); // create a timer that fires every 500 milliseconds, and when it fires, call the timer_callback() method on this object instance.
 
   }
   private:
@@ -40,7 +40,7 @@ public:
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv); //init ros
-  rclcpp::spin(std::make_shared<Publisher>()); //spin calls call callback functions
+  rclcpp::spin(std::make_shared<Talker>()); //spin calls call callback functions
   rclcpp::shutdown();
   return 0;
 }
--- src/exercise1/src/subscriber_node.cpp
+++ src/exercise1/src/subscriber_node.cpp
@@ -1,11 +1,35 @@
 
-#include <cstdio>
+#include <functional>
+#include <memory>
+#include "rclcpp/rclcpp.hpp"
 
-int main(int argc, char ** argv)
+#include "std_msgs/msg/int64.hpp"
+
+using std::placeholders::_1; //placeholder for arguments 
+
+class Listener: public rclcpp::Node
+// Write a subscriber node named listener that subscribes to the topic "first_test_topic" and that prints
+// the received value on the command line using RCLCPP_INFO(). Additionally, display the history of the value of first_test_topic using rqt.
 {
-  (void) argc;
-  (void) argv;
+  public:
+    Listener() :  Node("Listener") 
+    {
+    subscription_ = this->create_subscription<std_msgs::msg::Int64>("first_test_topic", 10, std::bind(&Listener::topic_callback, this, _1));
+    }
 
-  printf("hello world exercise1 package\n");
+  private: 
+  void topic_callback(const std_msgs::msg::Int64 & msg) const //we get an int message now 
+  {
+    RCLCPP_INFO(this->get_logger(), "I heard: '%ld'", msg.data);
+  }
+  rclcpp::Subscription<std_msgs::msg::Int64>::SharedPtr subscription_;
+
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<Listener>());
+  rclcpp::shutdown();
   return 0;
 }
